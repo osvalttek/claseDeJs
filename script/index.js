4549c341ba8dce6d2cd87d6a81ee814585ed990a
Osvaldito <osvaldo.devmate@gmail.com>
2022-10-25 20:09:10 -0300

    try, catch, throw, finaly, setTimeout, intro a asincronia

--- script/index.js
+++ script/index.js
@@ -26,22 +26,52 @@
 // saludar()
 // ---------------------------------------------------------------------------
 const form = document.querySelector("form")
-const input = document.querySelector("input")
+const nombre = document.querySelector("#nombre")
+const edad = document.querySelector("#edad")
 const p = document.querySelector("p")
 
-// form.addEventListener("submit",)
+
 form.onsubmit = (e) => {
     e.preventDefault()
     try {
-        if (!input.value) throw "El campo no puede estar vacio"
-        if (typeof (input.value) === "number") throw "Np podes ingresar numeros"
-        p.textContent = input.value
+        if (!nombre.value || !edad.value) throw "El campo no puede estar vacio"
+        if (edad.value < 18) throw "Sos menor de edad"
+        p.textContent = `Vienvenido!!! podes pasar ${nombre.value}, porque tenes ${edad.value}`
     } catch (e) {
         p.textContent = e
     } finally {
-        input.value = ""
+        nombre.value = ""
+        edad.value = ""
+        setTimeout(() => {
+            p.textContent = ""
+        }, 2000);
     }
+}
 
+function hola() {
+    // console.log("hola")
+     return "hola"
+}
+function chau() {
+    console.log("chau")
+}
+
+setTimeout(() => {
+    hola()
+}, 0);
+
+chau()
+
+let arr = ["a", "b", "c"]
+
+function up(params) {
+    return params.map((e) => e.toUpperCase())
+}
+
+function saludar() {
+    setTimeout(() => {
+        hola()
+    }, 2000);
 }
 
 
